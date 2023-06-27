aef2bb057479e9a5fd0d276f6b5ed0c0828ff7b2
Dylan Erdini <erdini.dylan@gmail.com>
2023-06-26 23:00:29 -0300

    Desabilitando el formulario del total

--- pages/total.js
+++ pages/total.js
@@ -1,16 +1,32 @@
-import useQuiosco from '../hooks/useQuiosco';
+import { useEffect, useCallback } from 'react';
 import Layout from '../layout/Layout';
+import useQuiosco from '../hooks/useQuiosco';
 
 export default function Total() {
 
 	const { pedido } = useQuiosco()
+
+	const comprobarPedido = useCallback(() => {
+		return pedido.length === 0
+	}, [pedido])
+
+	useEffect(() => {
+		comprobarPedido()
+	}, [pedido, comprobarPedido])
+
+	const colocarOrden = e => {
+		e.preventDefault()
+		console.log("Enviando Formulario")
+	}
 
 	return (
 		<Layout pagina={'Total y Confirmar Pedido'}>
 			<h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
 			<p className="text-2xl my-10">Confirma tu pedido a continuación</p>
 
-			<form>
+			<form
+				onSubmit={colocarOrden}
+			>
 				<div>
 					<label htmlFor="nombre" className="block uppercase text-slate-800 font-bold text-xl">
 						Nombre
@@ -29,9 +45,10 @@ export default function Total() {
 
 				<div className='mt-5'>
 					<input
-						type='button'
-						className='bg-indigo-600 hover:bg-indigo-800 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center cursor-pointer transition-all'
+						type='submit'
+						className={`${comprobarPedido() ? "bg-indigo-100" : "bg-indigo-600 hover:bg-indigo-800"}  w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center cursor-pointer transition-all`}
 						value="Confirmar Pedido"
+						disabled={comprobarPedido()}
 					/>
 				</div>
 			</form>
