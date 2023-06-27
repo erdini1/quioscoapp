5cf6f2a1eaf3eb9dade9dc1ad2354c2a7b8151b3
Dylan Erdini <erdini.dylan@gmail.com>
2023-06-26 22:40:23 -0300

    Mostrando el input en la pagina total

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,100 +1,100 @@
-import { useState, useEffect, createContext } from "react";
-import axios from "axios";
-import { toast } from "react-toastify"
-import { useRouter } from "next/router";
+import { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
-const QuioscoContext = createContext()
+const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
+	const [ categorias, setCategorias ] = useState([]);
+	const [ categoriaActual, setCategoriaActual ] = useState({});
+	const [ producto, setProducto ] = useState({});
+	const [ modal, setModal ] = useState(false);
+	const [ pedido, setPedido ] = useState([]);
 
-    const [categorias, setCategorias] = useState([])
-    const [categoriaActual, setCategoriaActual] = useState({})
-    const [producto, setProducto] = useState({})
-    const [modal, setModal] = useState(false)
-    const [pedido, setPedido] = useState([])
+	const router = useRouter();
 
-    const router = useRouter()
+	const obtenerCategorias = async () => {
+		const { data } = await axios('/api/categorias');
+		setCategorias(data);
+	};
+	// El siguiente UseEffect me trae las categorias de la api
+	useEffect(() => {
+		obtenerCategorias();
+	}, []);
 
-    const obtenerCategorias = async () => {
-        const { data } = await axios("/api/categorias")
-        setCategorias(data)
-    }
-    // El siguiente UseEffect me trae las categorias de la api
-    useEffect(() => {
-        obtenerCategorias()
-    }, [])
+	// El siguiente UseEffect resalta la primer categoria
+	useEffect(
+		() => {
+			setCategoriaActual(categorias[0]);
+		},
+		[ categorias ]
+	);
 
-    // El siguiente UseEffect resalta la primer categoria
-    useEffect(() => {
-        setCategoriaActual(categorias[0])
-    }, [categorias])
+	const handleClickCategoria = (id) => {
+		// filtra las categorias y me muestra la que coincide con el id indicado
+		const categoria = categorias.filter((c) => c.id === id);
+		setCategoriaActual(categoria[0]);
+		router.push('/');
+	};
 
-    const handleClickCategoria = id => {
-        // filtra las categorias y me muestra la que coincide con el id indicado
-        const categoria = categorias.filter(c => c.id === id)
-        setCategoriaActual(categoria[0])
-        router.push("/")
-    }
+	const handleSetProducto = (producto) => {
+		setProducto(producto);
+	};
 
-    const handleSetProducto = producto => {
-        setProducto(producto)
-    }
+	const handleChangeModal = () => {
+		setModal(!modal);
+	};
 
-    const handleChangeModal = () => {
-        setModal(!modal)
-    }
+	const handleAgregarPedido = ({ categoriaId, ...producto }) => {
+		if (pedido.some((productoState) => productoState.id === producto.id)) {
+			//Actualizar la cantidad
+			const pedidoActualizado = pedido.map(
+				(productoState) => (productoState.id === producto.id ? producto : productoState)
+			);
+			setPedido(pedidoActualizado);
+			toast.success('Guardado correctamente');
+		} else {
+			setPedido([ ...pedido, producto ]);
+			toast.success('Agregado al pedido');
+		}
 
-    const handleAgregarPedido = ({ categoriaId, ...producto }) => {
+		setModal(false);
+	};
 
-        if (pedido.some(productoState => productoState.id === producto.id)) {
+	const handleEditarCantidades = (id) => {
+		const productoActualizar = pedido.filter((producto) => producto.id === id);
+		setProducto(productoActualizar[0]);
+		setModal(!modal);
+	};
 
-            //Actualizar la cantidad
-            const pedidoActualizado = pedido.map(productoState => productoState.id === producto.id ? producto : productoState)
-            setPedido(pedidoActualizado)
-            toast.success("Guardado correctamente")
-        } else {
-            setPedido([...pedido, producto])
-            toast.success("Agregado al pedido")
-        }
+	const handleEliminarProducto = (id) => {
+		const productoActualizar = pedido.filter((producto) => producto.id !== id);
+		setPedido(productoActualizar);
+		toast.success('Producto eliminado del pedido');
+	};
 
-        setModal(false)
-    }
+	return (
+		<QuioscoContext.Provider
+			value={{
+				categorias,
+				handleClickCategoria,
+				categoriaActual,
+				producto,
+				handleSetProducto,
+				handleChangeModal,
+				modal,
+				handleAgregarPedido,
+				pedido,
+				handleEditarCantidades,
+				handleEliminarProducto
+			}}
+		>
+			{children}
+		</QuioscoContext.Provider>
+	);
+};
 
-    const handleEditarCantidades = id => {
-        const productoActualizar = pedido.filter(producto => producto.id === id)
-        setProducto(productoActualizar[0])
-        setModal(!modal)
-    }
+export { QuioscoProvider };
 
-    const handleEliminarProducto = id => {
-        const productoActualizar = pedido.filter(producto => producto.id !== id)
-        setPedido(productoActualizar)
-        toast.success("Producto eliminado del pedido")
-    }
-
-    return (
-        <QuioscoContext.Provider
-            value={{
-                categorias,
-                handleClickCategoria,
-                categoriaActual,
-                producto,
-                handleSetProducto,
-                handleChangeModal,
-                modal,
-                handleAgregarPedido,
-                pedido,
-                handleEditarCantidades,
-                handleEliminarProducto
-            }}
-        >
-            {children}
-        </QuioscoContext.Provider>
-    )
-}
-
-export {
-    QuioscoProvider
-}
-
-export default QuioscoContext
+export default QuioscoContext;
---
+++ pages/total.js
@@ -0,0 +1,40 @@
+import useQuiosco from '../hooks/useQuiosco';
+import Layout from '../layout/Layout';
+
+export default function Total() {
+
+	const { pedido } = useQuiosco()
+
+	return (
+		<Layout pagina={'Total y Confirmar Pedido'}>
+			<h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
+			<p className="text-2xl my-10">Confirma tu pedido a continuación</p>
+
+			<form>
+				<div>
+					<label htmlFor="nombre" className="block uppercase text-slate-800 font-bold text-xl">
+						Nombre
+					</label>
+
+					<input id="nombre" type="text" className="bg-gray-200 w-full lg:w-1/3 mt-3 p-2 rounded-md" />
+				</div>
+
+				<div className='mt-10'>
+					<p className='text-2xl'>Total a pagar: {""}
+						<span className='font-bold'>
+							$200
+						</span>
+					</p>
+				</div>
+
+				<div className='mt-5'>
+					<input
+						type='button'
+						className='bg-indigo-600 hover:bg-indigo-800 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center cursor-pointer transition-all'
+						value="Confirmar Pedido"
+					/>
+				</div>
+			</form>
+		</Layout>
+	);
+}
