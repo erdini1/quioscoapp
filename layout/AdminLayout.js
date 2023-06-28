619c7c0653753f5fcd57e3f11c2ea79ef5473aa7
Dylan Erdini <erdini.dylan@gmail.com>
2023-06-28 19:22:30 -0300

    Creando una pagina con las ordenes completadas y agregando router al adminLayout

--- layout/AdminLayout.js
+++ layout/AdminLayout.js
@@ -1,31 +1,65 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function AdminLayout({ children, pagina }) {
+
+  const router = useRouter()
+
+  const handleClickOrden = estado => {
+    if (estado === 0) {
+      router.push("/admin")
+    } else {
+      router.push("/admin/completas")
+    }
+  }
+
   return (
     <>
       <Head>
         <title>Café - {pagina}</title>
-        <meta name="description" content="Quosco Cafetería" />
+        <meta name="description" content="Quiosco Cafetería" />
       </Head>
 
       <div className="md:flex">
-            <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5 py-5">
-                <Image
-                    width={300}
-                    height={100}
-                    src="/assets/img/logo.svg"
-                    alt="imagen logotipo"
-                />
-            </aside>
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5 py-5">
+          <Image
+            width={300}
+            height={100}
+            src="/assets/img/logo.svg"
+            alt="imagen logotipo"
+          />
 
-            <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-                <div className="p-10">
-                    {children}
-                </div>
-            </main>
+          <nav className="mt-10">
+            <button
+              type="button"
+              className={`${router.pathname === "/admin" ? "bg-amber-400" : ""} p-5 text-2xl font-bold hover:cursor-pointer border hover:bg-amber-400 transition-all w-full }`}
+              onClick={() => handleClickOrden(0)}
+            >
+              Ordenes Incompletas
+            </button>
+            <button
+              type="button"
+              className={`${router.pathname === "/admin/completas" ? "bg-amber-400" : ""} p-5 w-full text-2xl font-bold hover:cursor-pointer border hover:bg-amber-400 transition-all }`}
+              onClick={() => handleClickOrden(1)}
+            >
+              Ordenes Completadas
+            </button>
+          </nav>
+
+          {/* TODO: AGREGAR UNA PAGINA CON ORDENES COMPLETADAS Y LAS QUE NO ESTAN COMPLETADAS. VER SI HAGO COMPONENTES NUEVOS */}
+          {/* TODO: Agregar la funcionalidad de un filtro en el listado de ordenes, ver de agregarlo al layout */}
+          {/* TODO: Ver que mas le puedo agregar */}
+
+        </aside>
+
+        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
+          <div className="p-10">
+            {children}
+          </div>
+        </main>
       </div>
       <ToastContainer />
     </>
